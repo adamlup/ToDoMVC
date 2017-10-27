7ba47e9db296ad0ea0e00ef2be85a770b362c04e
adamlup <adamlupi@gmail.com>
2017-10-27 14:20:47 +0200

    test, working add todo item

--- controllers/Todo_controllers.py
+++ controllers/Todo_controllers.py
@@ -1,2 +1,30 @@
+from Todo_models import Todo
+from Todo_views import *
+
 def start_controller():
-    pass
+    print_menu()
+    choice = get_choice()
+    while choice.isdigit():
+        if choice == '1':
+            todo_item = add_todo_item()
+            Todo.all_todo_items.append(todo_item)
+            break
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            pass
+        elif choice == '4':
+            pass
+        elif choice == '5':
+            print_todo_items_list(Todo.all_todo_items)
+        elif choice == '6':
+            pass
+    
+def add_todo_item():
+    name = get_item_name()
+    description = get_item_description()
+    return name, description
+
+start_controller()
---
+++ controllers/main.py
@@ -0,0 +1,8 @@
+from Todo_controllers import *
+
+def main():
+    while True:
+        start_controller()
+
+if __name__ == '__main__':
+    main()
