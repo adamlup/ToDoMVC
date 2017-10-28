8058d1b30ba9f4f8c31683bff0d2897d7f4fd066
adamlup <adamlupi@gmail.com>
2017-10-28 17:21:31 +0200

    remove not needed function

--- controllers/Todo_models.py
+++ controllers/Todo_models.py
@@ -6,6 +6,3 @@ class Todo():
         self.name = name
         self.description = description
         self.is_done = False
-
-    def __str__(self):
-        return 'item name -->', str(self.name), 'item status -->', str(self.is_done), 'item description -->', str(self.description)
