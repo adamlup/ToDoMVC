c91d1a34679151d9952e039e265fc590d293eee0
adamlup <adamlupi@gmail.com>
2017-10-28 17:20:07 +0200

    make bulletproof controller

--- controllers/Todo_controllers.py
+++ controllers/Todo_controllers.py
@@ -1,40 +1,29 @@
 from Todo_models import Todo
 from Todo_views import *
 import os
+import sys
+
 
 def start_controller():
     print_menu()
     choice = get_choice()
+    os.system('clear')
     while choice.isdigit():
         if choice == '1':
             todo_item = add_todo_item()
-            #os.system('clear')
-            #print_todo_details(todo_item)
             Todo.all_todo_items.append(todo_item)
             break
         elif choice == '2':
-            print_todo_items_list(Todo.all_todo_items)
-            choice_item_by_index = choice_item_by_index()
-            for i in Todo.all_todo_items:
-                if i == Todo.all_todo_items[choice_item_by_index]:
-                    i.name = get_item_name()
-                    i.description = get_item_description()
-                    break
+            modify_item()
             break
 
         elif choice == '3':
-            choice_item_by_index = get_item_index()
-            for i in Todo.all_todo_items:
-                if i == Todo.all_todo_items[choice_item_by_index]:
-                    Todo.all_todo_items.remove(i)
+            remove_item()
             break
 
         elif choice == '4':
-            choice_item_by_index = get_item_index()
-            for i in Todo.all_todo_items:
-                if i == Todo.all_todo_items[choice_item_by_index]:
-                    i.is_done = True
-                    return
+            change_item_status()
+            break
 
         elif choice == '5':
             print_todo_items_list(Todo.all_todo_items)
@@ -47,20 +36,71 @@ def start_controller():
                 index += 1
             break
 
+        elif choice == '7':
+            sys.exit(0)
+
+        else:
+            return invalid_input()
 
 
 def add_todo_item():
     while True:
         name = get_item_name()
         if len(name) > 20:
-            print(item_name_too_long())
+            item_name_too_long()
             continue
         description = get_item_description()
         if len(description) > 150:
-            print(item_description_too_long())
+            item_description_too_long()
             continue
         else:
             return Todo(name, description)
 
 
-start_controller()
+def modify_item():
+    while True:
+        choice_item_by_index = get_item_index()
+        if choice_item_by_index.isdigit():
+            choice_item_by_index = int(choice_item_by_index)
+            if choice_item_by_index in range(len(Todo.all_todo_items)):
+                for i in Todo.all_todo_items:
+                    if i == Todo.all_todo_items[choice_item_by_index]:
+                        i.name = get_item_name()
+                        i.description = get_item_description()
+                break
+            else:
+                return invalid_input()
+        else:
+            return invalid_input()
+
+
+def remove_item():
+    while True:
+        choice_item_by_index = get_item_index()
+        if choice_item_by_index.isdigit():
+            choice_item_by_index = int(choice_item_by_index)
+            if choice_item_by_index in range(len(Todo.all_todo_items)):
+                for i in Todo.all_todo_items:
+                    if i == Todo.all_todo_items[choice_item_by_index]:
+                        Todo.all_todo_items.remove(i)
+                break
+            else:
+                return invalid_input()
+        else:
+            return invalid_input()
+
+
+def change_item_status():
+    while True:
+        choice_item_by_index = get_item_index()
+        if choice_item_by_index.isdigit():
+            choice_item_by_index = int(choice_item_by_index)
+            if choice_item_by_index in range(len(Todo.all_todo_items)):
+                for i in Todo.all_todo_items:
+                    if i == Todo.all_todo_items[choice_item_by_index]:
+                        i.is_done = True
+                break
+            else:
+                return invalid_input()
+        else:
+            return invalid_input()
